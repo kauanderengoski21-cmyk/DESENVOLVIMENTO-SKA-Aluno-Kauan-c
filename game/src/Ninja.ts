d62e23d8cfffa2e3de6a4189228d1ad4941fb44f
KauanC. <kauanderengoski21@gmail.com>
2026-04-29 21:17:02 -0300

    ta ai

---
+++ game/src/Ninja.ts
@@ -0,0 +1,44 @@
+import { personagem } from "./personagem.ts";
+
+export class Ninja extends personagem {
+      
+    constructor(nome: string, forca: number, vida: number) {
+        super(nome, forca, vida);
+    }
+
+    
+    public atacar(persona: personagem): void {
+        console.log(`${this.nome} atacou com katana ${persona.nome}`);
+        persona.sofrerAtaque(this.forca);
+    }
+
+    
+    public shuriken (persona: personagem): void {
+        const dano = this.forca * 1.5;
+        console.log(`${this.nome} usou usou sua shurikens ${persona.nome}`);
+        persona.sofrerAtaque(dano);
+    }
+
+    
+    public BleedSword (persona: personagem): void {
+        const critico = Math.random() < 0.3; 
+        const dano = critico ? this.forca * 2 : this.forca;
+
+        console.log(`${this.nome} tentou um Ataque Crítico em ${persona.nome}`);
+
+        if (critico) {
+            console.log("CRÍTICO!");
+        }
+
+        persona.sofrerAtaque(dano);
+    }
+
+    
+    public curar(valor: number): void {
+        const vidaAntes = this.vida;
+        this.vida = Math.min(this.vida + valor, );
+
+        console.log(`${this.nome} curou ${this.vida - vidaAntes} HP`);
+    }
+
+}
---
+++ game/src/jpguinho.ts
@@ -0,0 +1,9 @@
+import { Serjao } from "../serjao.ts";
+import { Ninja } from "../Ninja.ts";
+import { jogo } from "../jogo.ts";
+
+let ninja: Ninja = new Ninja ("ninja", 1,450);
+let serjao:Serjao = new Serjao ("serjao", 1, 450);
+
+let Jogo : jogo = new jogo ();
+Jogo.iniciar (ninja, serjao);
